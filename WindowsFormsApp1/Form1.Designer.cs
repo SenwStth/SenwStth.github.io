07310069443c1e007540812076a10ea0619691d1
SenwStth <bromidhlora@gmail.com>
2026-03-16 12:03:49 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelBase = new System.Windows.Forms.Label();
+            this.comboBoxBase = new System.Windows.Forms.ComboBox();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.labelAllowedChars = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.labelResultTitle = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.pictureBoxFormula = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFormula)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.labelTitle.Location = new System.Drawing.Point(200, 20);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(580, 24);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Перевод чисел из n-ой системы счисления в десятичную";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelBase
+            // 
+            this.labelBase.AutoSize = true;
+            this.labelBase.Location = new System.Drawing.Point(50, 160);
+            this.labelBase.Name = "labelBase";
+            this.labelBase.Size = new System.Drawing.Size(135, 13);
+            this.labelBase.TabIndex = 2;
+            this.labelBase.Text = "Выберите систему (2-16):";
+            // 
+            // comboBoxBase
+            // 
+            this.comboBoxBase.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBase.Location = new System.Drawing.Point(200, 157);
+            this.comboBoxBase.Name = "comboBoxBase";
+            this.comboBoxBase.Size = new System.Drawing.Size(150, 21);
+            this.comboBoxBase.TabIndex = 3;
+            this.comboBoxBase.SelectedIndexChanged += new System.EventHandler(this.comboBoxBase_SelectedIndexChanged);
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.AutoSize = true;
+            this.labelNumber.Location = new System.Drawing.Point(50, 200);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(84, 13);
+            this.labelNumber.TabIndex = 4;
+            this.labelNumber.Text = "Введите число:";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(200, 197);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(150, 20);
+            this.textBoxNumber.TabIndex = 5;
+            this.textBoxNumber.TextChanged += new System.EventHandler(this.textBoxNumber_TextChanged);
+            this.textBoxNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxNumber_KeyPress);
+            // 
+            // labelAllowedChars
+            // 
+            this.labelAllowedChars.AutoSize = true;
+            this.labelAllowedChars.ForeColor = System.Drawing.Color.Gray;
+            this.labelAllowedChars.Location = new System.Drawing.Point(360, 200);
+            this.labelAllowedChars.Name = "labelAllowedChars";
+            this.labelAllowedChars.Size = new System.Drawing.Size(124, 13);
+            this.labelAllowedChars.TabIndex = 6;
+            this.labelAllowedChars.Text = "Допустимые символы:";
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Enabled = false;
+            this.buttonCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonCalculate.Location = new System.Drawing.Point(200, 240);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(150, 35);
+            this.buttonCalculate.TabIndex = 7;
+            this.buttonCalculate.Text = "Перевести";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // labelResultTitle
+            // 
+            this.labelResultTitle.AutoSize = true;
+            this.labelResultTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.labelResultTitle.Location = new System.Drawing.Point(50, 300);
+            this.labelResultTitle.Name = "labelResultTitle";
+            this.labelResultTitle.Size = new System.Drawing.Size(149, 17);
+            this.labelResultTitle.TabIndex = 8;
+            this.labelResultTitle.Text = "Результат (10-ая):";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.labelResult.ForeColor = System.Drawing.Color.Blue;
+            this.labelResult.Location = new System.Drawing.Point(200, 300);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 20);
+            this.labelResult.TabIndex = 9;
+            // 
+            // pictureBoxFormula
+            // 
+            this.pictureBoxFormula.Location = new System.Drawing.Point(150, 60);
+            this.pictureBoxFormula.Name = "pictureBoxFormula";
+            this.pictureBoxFormula.Size = new System.Drawing.Size(540, 80);
+            this.pictureBoxFormula.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxFormula.TabIndex = 1;
+            this.pictureBoxFormula.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 400);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelResultTitle);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.labelAllowedChars);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.labelNumber);
+            this.Controls.Add(this.comboBoxBase);
+            this.Controls.Add(this.labelBase);
+            this.Controls.Add(this.pictureBoxFormula);
+            this.Controls.Add(this.labelTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Перевод систем счисления - Вариант 14";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFormula)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.PictureBox pictureBoxFormula;
+        private System.Windows.Forms.Label labelBase;
+        private System.Windows.Forms.ComboBox comboBoxBase;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label labelAllowedChars;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label labelResultTitle;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
