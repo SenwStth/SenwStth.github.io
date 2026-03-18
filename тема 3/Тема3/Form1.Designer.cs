7357a3132cebb0481d84442011cfa4d20cfd5d18
SenwStth <bromidhlora@gmail.com>
2026-03-18 10:54:01 +0300

    Add files via upload

---
+++ тема 3/Тема3/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button btnStart;
+        private Button btnStop;
+        private Button btnSettings;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // btnStart
+            this.btnStart.Location = new System.Drawing.Point(20, 20);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(90, 30);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+
+            // btnStop
+            this.btnStop.Location = new System.Drawing.Point(120, 20);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(90, 30);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Enabled = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+
+            // btnSettings
+            this.btnSettings.Location = new System.Drawing.Point(220, 20);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(90, 30);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.Text = "Настройки";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вариант 14: Движение квадрата с трансформацией";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ тема 3/Тема3/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Параметры фигуры
+        private RectangleF shapeRect;           // Прямоугольник фигуры
+        private float shapeWidth = 50;          // Ширина фигуры
+        private float shapeHeight = 50;         // Высота фигуры
+        private Color shapeColor = Color.Blue;  // Цвет фигуры
+
+        // Параметры движения
+        private float stepX = 2;                 // Шаг движения по X
+        private float minX = 10;                  // Минимальная позиция по X
+        private float maxX;                        // Максимальная позиция по X
+
+        // Параметры трансформации
+        private float currentHeight = 50;         // Текущая высота
+        private float minHeight = 50;              // Минимальная высота (квадрат)
+        private float maxHeight = 100;             // Максимальная высота (прямоугольник)
+        private float heightStep = 0.5f;           // Шаг изменения высоты
+        private bool expanding = true;              // Растягиваемся или сжимаемся
+
+        // Таймер
+        private Timer animationTimer;
+
+        // Флаг для завершения программы
+        private bool isRunning = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Настройка таймера
+            animationTimer = new Timer();
+            animationTimer.Interval = 30;
+            animationTimer.Tick += AnimationTimer_Tick;
+
+            // Подписка на события клавиш
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+
+            // Инициализация позиции
+            InitializeShape();
+        }
+
+        private void InitializeShape()
+        {
+            // Начальная позиция - левая часть формы
+            minX = shapeWidth / 2 + 5;
+            maxX = this.ClientSize.Width - shapeWidth / 2 - 5;
+
+            float startX = minX;
+            float startY = this.ClientSize.Height / 2;
+
+            shapeRect = new RectangleF(
+                startX - shapeWidth / 2,
+                startY - currentHeight / 2,
+                shapeWidth,
+                currentHeight
+            );
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            // Движение по горизонтали
+            shapeRect.X += stepX;
+
+            // Проверка достижения границ
+            if (shapeRect.X <= minX)
+            {
+                shapeRect.X = minX;
+                stepX = Math.Abs(stepX);
+            }
+            else if (shapeRect.X + shapeRect.Width >= maxX + shapeRect.Width / 2)
+            {
+                shapeRect.X = maxX + shapeRect.Width / 2 - shapeRect.Width;
+                stepX = -Math.Abs(stepX);
+            }
+
+            // Трансформация высоты
+            if (expanding)
+            {
+                currentHeight += heightStep;
+                if (currentHeight >= maxHeight)
+                {
+                    currentHeight = maxHeight;
+                    expanding = false;
+                }
+            }
+            else
+            {
+                currentHeight -= heightStep;
+                if (currentHeight <= minHeight)
+                {
+                    currentHeight = minHeight;
+                    expanding = true;
+                }
+            }
+
+            // Обновление прямоугольника
+            shapeRect.Height = currentHeight;
+            shapeRect.Y = this.ClientSize.Height / 2 - currentHeight / 2;
+
+            // Перерисовка
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Рисование фигуры
+            using (SolidBrush brush = new SolidBrush(shapeColor))
+            {
+                e.Graphics.FillRectangle(brush, shapeRect);
+            }
+
+            // Рисование рамки
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                e.Graphics.DrawRectangle(pen, Rectangle.Round(shapeRect));
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Завершение при нажатии любой клавиши
+            if (isRunning)
+            {
+                animationTimer.Stop();
+                isRunning = false;
+                this.Close();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.ClientSize.Width > 0 && this.ClientSize.Height > 0)
+            {
+                maxX = this.ClientSize.Width - shapeWidth / 2 - 10;
+
+                if (shapeRect != null)
+                {
+                    if (shapeRect.X < minX)
+                        shapeRect.X = minX;
+                    if (shapeRect.X + shapeRect.Width > this.ClientSize.Width - minX)
+                        shapeRect.X = this.ClientSize.Width - shapeRect.Width - minX;
+
+                    shapeRect.Y = this.ClientSize.Height / 2 - currentHeight / 2;
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            animationTimer.Start();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            animationTimer.Stop();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            Form2 settingsForm = new Form2(shapeColor, animationTimer.Interval);
+
+            if (settingsForm.ShowDialog() == DialogResult.OK)
+            {
+                shapeColor = settingsForm.SelectedColor;
+                animationTimer.Interval = settingsForm.Speed;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            animationTimer.Start();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+    }
+}
